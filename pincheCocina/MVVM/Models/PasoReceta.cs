6dd24bb86c845db60dbc5d3bb2fac769876e0f93
Pablo RG <pabloromo169@gmail.com>
2026-01-19 13:52:13 +0100

    Añadidas clases del modelo

---
+++ pincheCocina/MVVM/Models/PasoReceta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pincheCocina.MVVM.Models
+{
+    public class PasoReceta
+    {
+        public string Accion { get; set; }
+        public List<Ingrediente> Ingredientes { get; set; } = new();
+
+        public PasoReceta(string accion_)
+        {
+            Accion = accion_;
+        }
+
+        public bool AddAccion(string accion_)
+        {
+            if (accion_ != null)
+            {
+                Accion = accion_;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool addIngrediente(Ingrediente ingrediente)
+        {
+            if (ingrediente != null)
+            {
+                Ingredientes.Add(ingrediente);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ pincheCocina/MVVM/Models/Receta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pincheCocina.MVVM.Models
+{
+    public class Receta
+    {
+        public string Nombre { get; set; }
+        public List<PasoReceta> Pasos { get; set; } = new();
+
+        public Receta(string nombre_)
+        {
+            Nombre = nombre_;
+        }
+
+        public Receta(string nombre_, List<PasoReceta> Pasos_)
+        {
+            Nombre = nombre_;
+            Pasos = Pasos_;
+        }
+    }
+}
